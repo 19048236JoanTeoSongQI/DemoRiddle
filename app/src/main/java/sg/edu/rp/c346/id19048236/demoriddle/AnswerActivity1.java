2e04e17f480d739bc66521b72135c1df7390a6be
19048236JoanTeoSongQI <19048236@myrp.edu.sg>
2021-05-24 13:58:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19048236/demoriddle/AnswerActivity1.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id19048236.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity1 extends AppCompatActivity {
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer1);
+
+        Intent intentReceived=getIntent();
+        String questionSelected= intentReceived.getStringExtra("Question");
+
+        tvAnswer=findViewById(R.id.textView1);
+        if(questionSelected.equalsIgnoreCase("q1")){
+            tvAnswer.setText(questionSelected+" answer is :Queue");
+
+        }else if(questionSelected.equalsIgnoreCase("q2")){
+            tvAnswer.setText(questionSelected+"  answer is: Gone");
+
+        }
+
+
+    }
+}
